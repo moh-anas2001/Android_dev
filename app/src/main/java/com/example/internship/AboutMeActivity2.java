bcfd7a346cbbefd64e31ec58f3eb124bad848c7f
Mohamed Anas <mohamedanascse@gmail.com>
2023-07-23 23:04:29 +0530

    90

---
+++ app/src/main/java/com/example/internship/AboutMeActivity2.java
@@ -0,0 +1,30 @@
+package com.example.internship;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class AboutMeActivity2 extends AppCompatActivity {
+
+    private ImageButton backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_me2);
+
+        backButton = findViewById(R.id.back_button);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AboutMeActivity2.this, "back Button Clicked", Toast.LENGTH_SHORT).show();
+                onBackPressed();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/internship/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.internship;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listview;
+    List<String> listViewValues;
+    ArrayList<String> listviewdetails = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listview=(ListView) findViewById(R.id.mobile_list);
+
+
+        try {
+            JSONObject jsonObject=new JSONObject(loadJsonFile());
+            JSONArray jsonArray=jsonObject.getJSONArray("studentdetails");
+            listViewValues = new ArrayList<String>();
+            for(int i=0; i<jsonArray.length();i++)
+            {
+                JSONObject obj=jsonArray.getJSONObject(i);
+                String name=obj.getString("name");
+                listViewValues.add(name);
+            }
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.displayname, R.id.label,listViewValues);
+            listview.setAdapter(adapter);
+            listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    Toast.makeText(MainActivity.this, "You clicked", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, AboutMeActivity.class);
+                    startActivity(intent);
+
+                }
+            });
+
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String loadJsonFile() throws IOException {
+        String json=null;
+        InputStream inputStream=this.getAssets().open("studentdetails.json");
+        int size=inputStream.available();
+        byte[] byteArray=new byte[size];
+        inputStream.read(byteArray);
+        inputStream.close();
+        json=new String(byteArray, "UTF-8");
+        return json;
+    }
+}
